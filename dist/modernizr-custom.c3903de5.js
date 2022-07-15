719b2eb55b7cba756483ed5f43cd19fe3759bd34
Dominic Lee <dominic@radicaltech.co.za>
2022-07-15 14:44:20 +0200

    Friday demo before sound

--- dist/modernizr-custom.c3903de5.js
+++ dist/modernizr-custom.c3903de5.js
@@ -281,7 +281,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "13530" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "12305" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
